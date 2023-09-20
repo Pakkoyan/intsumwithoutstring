36594e853240567fc59ed8ac6513dfad1fa4e59f
Pakkoyan <maksimenkobogdan12@gmail.com>
2023-09-20 11:01:23 +0600

    FirstAndLast

---
+++ intsumwithoutstring/Program.cs
@@ -0,0 +1,52 @@
+﻿
+int num = Int32.Parse(Console.ReadLine());
+int sum = 0;
+
+if (num / 10 != 0)
+{
+    sum += num % 10;
+    num /= 10;
+}
+if (num / 10 != 0)
+{
+    sum += num % 10;
+    num /= 10;
+}
+if (num / 10 != 0)
+{
+    sum += num % 10;
+    num /= 10;
+}
+if (num / 10 != 0)
+{
+    sum += num % 10;
+    num /= 10;
+}
+if (num / 10 != 0)
+{
+    sum += num % 10;
+    num /= 10;
+}
+if (num / 10 != 0)
+{
+    sum += num % 10;
+    num /= 10;
+}
+if (num / 10 != 0)
+{
+    sum += num % 10;
+    num /= 10;
+}
+if (num / 10 != 0)
+{
+    sum += num % 10;
+    num /= 10;
+}
+if (num / 10 != 0)
+{
+    sum += num % 10;
+    num /= 10;
+}
+
+Console.WriteLine(sum + num);
+
